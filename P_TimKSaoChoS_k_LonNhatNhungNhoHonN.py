1d281d6d0789005ad16171c7486796fd890e24cd
TamNhien <canhthien91@gmail.com>
2024-03-25 20:44:17 +0700

    Add project files.

---
+++ P_TimKSaoChoS_k_LonNhatNhungNhoHonN.py
@@ -0,0 +1,8 @@
+﻿n = int(input("Nhập vào số nguyên dương n : "))
+
+k, S = 0, 0
+while (S := S + (k := k + 1)) <= n:
+    pass
+
+print("Giá trị của k sao cho S(k) lớn nhất nhưng nhỏ hơn n là : ", k - 1)
+
